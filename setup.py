1dd428ffc697a1515b1e599ff6575fc0d6241b28
blake-armstrong <blakearmstrong21@gmail.com>
2024-02-05 15:33:10 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="mustard",
+    description="Mulit-State Empirical Reactive Descriptions using MSEVB",
+    author="Blake Armstrong",
+    packages=find_packages()
+)
